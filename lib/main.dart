7a32e0cb8f7c75cf47b89ab1a83a07823eb7480a
Immadominion <immadominion@gmail.com>
2023-05-06 21:56:39 +0100

    checking smfin, went back two commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modern_login_page/views/sign_up.dart';
 import 'package:modern_login_page/views/log_in.dart';
+//import 'dart:ui' as ui;
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:modern_login_page/firebase_options.dart';
+import 'package:modern_login_page/views/snap_error.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -21,113 +28,132 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(202, 235, 35, 35),
-        title: const Center(
-            child: Text(
-          'Clever Modern Login Page',
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 20.0,
-            fontFamily: 'PoltawaskiNowy',
-          ),
-        )),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: ColoredBox(
-        color: const Color.fromARGB(255, 255, 235, 224),
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            PreferredSize(
-              preferredSize:
-                  Size(screenSize.width / 0.5, screenSize.height / 0.5),
-              child: const Image(image: AssetImage('assets/_image_modern.jpg')),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(202, 235, 35, 35),
+              title: const Center(
+                  child: Text(
+                'Clever Modern Login Page',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20.0,
+                  fontFamily: 'PoltawaskiNowy',
+                ),
+              )),
             ),
-            Center(
-              child: Column(
-                children: [
-                  Center(
-                    child: SizedBox(
-                      height: screenSize.height / 1.3,
-                      width: screenSize.width / 2,
-                    ),
+            body: ColoredBox(
+              color: const Color.fromARGB(255, 255, 235, 224),
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  PreferredSize(
+                    preferredSize:
+                        Size(screenSize.width / 0.5, screenSize.height / 0.5),
+                    child: const Image(
+                        image: AssetImage('assets/_image_modern.jpg')),
                   ),
                   Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Column(
                       children: [
-                        // SizedBox(
-                        //   width: screenSize.width / 7,
-                        // ),
-                        Container(
-                          width: 99.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(223, 244, 67, 54),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Center(
-                            child: InkWell(
-                              onTap: () {
-                                // _LoginPageState().myFocusNode.requestFocus();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const SignInPage()),
-                                );
-                              },
-                              child: const Text(
-                                'Sign Up',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15.0,
-                                  fontFamily: 'PoltawaskiNowy',
-                                ),
-                              ),
-                            ),
+                        Center(
+                          child: SizedBox(
+                            height: screenSize.height / 1.3,
+                            width: screenSize.width / 2,
                           ),
                         ),
-                        SizedBox(
-                          width: screenSize.width / 9,
-                        ),
-                        Container(
-                          width: 99.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(164, 237, 166, 52),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Center(
-                            child: InkWell(
-                              onTap: () {
-                                // _LoginPageState().myFocusNode.requestFocus();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const LogInPage()),
-                                );
-                              },
-                              child: const Text(
-                                'Log in',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15.0,
-                                  fontFamily: 'PoltawaskiNowy',
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // SizedBox(
+                              //   width: screenSize.width / 7,
+                              // ),
+                              Container(
+                                width: 99.0,
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(223, 244, 67, 54),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Center(
+                                  child: InkWell(
+                                    onTap: () {
+                                      // _LoginPageState().myFocusNode.requestFocus();
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const SignInPage()),
+                                      );
+                                    },
+                                    child: const Text(
+                                      'Sign Up',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 15.0,
+                                        fontFamily: 'PoltawaskiNowy',
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
+                              SizedBox(
+                                width: screenSize.width / 9,
+                              ),
+                              Container(
+                                width: 99.0,
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(164, 237, 166, 52),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Center(
+                                  child: InkWell(
+                                    onTap: () {
+                                      // _LoginPageState().myFocusNode.requestFocus();
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const LogInPage()),
+                                      );
+                                    },
+                                    child: const Text(
+                                      'Log in',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 15.0,
+                                        fontFamily: 'PoltawaskiNowy',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return const SnapshotErrorPage();
+        } else {
+          return const SpinKitWaveSpinner(
+            color: Color.fromARGB(255, 244, 77, 74),
+          );
+        }
+      },
     );
   }
 }
--- lib/views/log_in.dart
+++ lib/views/log_in.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
-import 'package:firebase_core/firebase_core.dart';
+
 // import 'package:flutter/services.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:modern_login_page/firebase_options.dart';
-import 'package:modern_login_page/views/snap_error.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LogInPage extends StatefulWidget {
@@ -40,158 +37,137 @@ class _LogInPageState extends State<LogInPage> {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
     var screenSize = MediaQuery.of(context).size;
-    return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              body: ColoredBox(
-                color: const Color.fromARGB(255, 255, 235, 224),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    PreferredSize(
-                      preferredSize:
-                          Size(screenSize.width / 0.5, screenSize.height / 0.5),
-                      child: const Image(
-                          image: AssetImage('assets/_image_modern.jpg')),
-                    ),
-                    ClipRect(
-                      child: BackdropFilter(
-                        filter: ui.ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                        child: Container(
-                          // width: screenSize.width / 2,
-                          // height: screenSize.height / 1.5,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade200.withOpacity(0.5),
+    return Scaffold(
+      body: ColoredBox(
+        color: const Color.fromARGB(255, 255, 235, 224),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            PreferredSize(
+              preferredSize:
+                  Size(screenSize.width / 0.5, screenSize.height / 0.5),
+              child: const Image(image: AssetImage('assets/_image_modern.jpg')),
+            ),
+            ClipRect(
+              child: BackdropFilter(
+                filter: ui.ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200.withOpacity(0.5),
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: screenSize.width / 1.5,
+                      height: screenSize.height / 2.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: screenSize.width / 10,
+                            height: screenSize.height / 50,
                           ),
-                          child: Center(
-                            child: Container(
-                              width: screenSize.width / 1.5,
-                              height: screenSize.height / 2.0,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.white,
-                              ),
-                              child: Column(
-                                children: [
-                                  SizedBox(
-                                    width: screenSize.width / 10,
-                                    height: screenSize.height / 50,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: TextField(
-                                      controller: _gmailTextController,
-                                      autofocus: true,
-                                      focusNode: myFocusNode,
-                                      keyboardType: TextInputType.emailAddress,
-                                      decoration: const InputDecoration(
-                                        labelText: 'Gmail',
-                                        border: OutlineInputBorder(),
-                                        hintText: 'Enter gmail here',
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: screenSize.width / 7,
-                                    height: screenSize.height / 120,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: TextField(
-                                      controller: _passTextController,
-                                      obscureText: true,
-                                      keyboardType:
-                                          TextInputType.visiblePassword,
-                                      decoration: const InputDecoration(
-                                        labelText: 'Password',
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide(
-                                            width: 2.0,
-                                            style: BorderStyle.solid,
-                                            color: Color.fromARGB(
-                                                255, 200, 200, 200),
-                                          ),
-                                        ),
-                                        hintText: 'Enter password here',
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: screenSize.width / 2,
-                                    height: screenSize.height / 25,
-                                  ),
-                                  InkWell(
-                                    onTap: () async {
-                                      final userEmail =
-                                          _gmailTextController.text;
-                                      final userPassword =
-                                          _passTextController.text;
-                                      final userCredential = await FirebaseAuth
-                                          .instance
-                                          .signInWithEmailAndPassword(
-                                        email: userEmail,
-                                        password: userPassword,
-                                      );
-                                      print(userCredential);
-                                    },
-                                    child: Container(
-                                      width: screenSize.width / 2.5,
-                                      height: screenSize.height / 15,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.red[400],
-                                      ),
-                                      child: const Center(
-                                        child: Text(
-                                          'Log In',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 15.0,
-                                            fontFamily: 'PoltawaskiNowy',
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: screenSize.width / 2,
-                                    height: screenSize.height / 14,
-                                  ),
-                                  const Text(
-                                    'Log in with gmail',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 10.0,
-                                      fontFamily: 'PoltawaskiNowy',
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: screenSize.width / 10,
-                                    height: screenSize.height / 150,
-                                  ),
-                                ],
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: TextField(
+                              controller: _gmailTextController,
+                              autofocus: true,
+                              focusNode: myFocusNode,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                labelText: 'Gmail',
+                                border: OutlineInputBorder(),
+                                hintText: 'Enter gmail here',
                               ),
                             ),
                           ),
-                        ),
+                          SizedBox(
+                            width: screenSize.width / 7,
+                            height: screenSize.height / 120,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: TextField(
+                              controller: _passTextController,
+                              obscureText: true,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 2.0,
+                                    style: BorderStyle.solid,
+                                    color: Color.fromARGB(255, 200, 200, 200),
+                                  ),
+                                ),
+                                hintText: 'Enter password here',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: screenSize.width / 2,
+                            height: screenSize.height / 25,
+                          ),
+                          InkWell(
+                            onTap: () async {
+                              final userEmail = _gmailTextController.text;
+                              final userPassword = _passTextController.text;
+                              final userCredential = await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                email: userEmail,
+                                password: userPassword,
+                              );
+                              print(userCredential);
+                            },
+                            child: Container(
+                              width: screenSize.width / 2.5,
+                              height: screenSize.height / 15,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.red[400],
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Log In',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 15.0,
+                                    fontFamily: 'PoltawaskiNowy',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: screenSize.width / 2,
+                            height: screenSize.height / 14,
+                          ),
+                          const Text(
+                            'Log in with gmail',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 10.0,
+                              fontFamily: 'PoltawaskiNowy',
+                            ),
+                          ),
+                          SizedBox(
+                            width: screenSize.width / 10,
+                            height: screenSize.height / 150,
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
-            );
-          } else if (snapshot.hasError) {
-            return const SnapshotErrorPage();
-          } else {
-            return const SpinKitWaveSpinner(
-              color: Color.fromARGB(255, 244, 77, 74),
-            );
-          }
-        });
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
